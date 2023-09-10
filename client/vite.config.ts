67bf753086f83e20e5f566aff8f77852f81b4436
jaybell <jaycooperbell@gmail.com>
2023-09-10 14:21:57 -0700

    override routes.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => ({
               include: ['/*'],
               exclude: ['/index.html'],
             },
+            defaultRoutes: false,
           },
         },
       },
