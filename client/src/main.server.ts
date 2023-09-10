821aed01987a1c48b606752e6991c1febe29f193
jaybell <jaycooperbell@gmail.com>
2023-09-10 14:01:17 -0700

    call parse

--- client/src/main.server.ts
+++ client/src/main.server.ts
@@ -5,6 +5,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import{parse} from 'url'
 
 if (import.meta.env.PROD) {
   enableProdMode();
@@ -13,11 +14,12 @@ if (import.meta.env.PROD) {
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default async function render(url: string, document: string, ...args: any[]) {
-  console.log('url', url);
+  console.log('url', url, parse( `https://jaybell-brandons-config.spartan-test.pages.dev${url}`));
   console.log('document', document);
   console.log('args', args);
   const html = await renderApplication(bootstrap, {
     document,
+    url: `https://jaybell-brandons-config.spartan-test.pages.dev${url}`,
   });
   return html;
 }
