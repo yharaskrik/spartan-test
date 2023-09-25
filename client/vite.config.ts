cbbcb90250ea0f9bf62120902f81a4bc93883a26
jaybell <jaycooperbell@gmail.com>
2023-09-24 22:09:22 -0400

    build: use other projects config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,70 +1,74 @@
 /// <reference types="vitest" />
 
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import analog from '@analogjs/platform';
 import { resolve } from 'node:path';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  publicDir: 'src/public',
-  build: {
-    target: ['es2020'],
-  },
-  ssr: {
-    noExternal: ['xhr2'],
-  },
-  resolve: {
-    mainFields: ['module'],
-  },
-  plugins: [
-    analog({
-      nitro: {
-        output: Object.assign({
-          dir: resolve(process.cwd(), 'dist/client/analog'),
-          publicDir: resolve(process.cwd(), 'dist/client/analog'),
-        }),
+export default defineConfig(({ mode }) => {
+  return {
+    publicDir: 'src/public',
+    build: {
+      target: ['es2020'],
+    },
+    ssr: {
+      noExternal: ['xhr2'],
+    },
+    resolve: {
+      mainFields: ['module'],
+    },
+    plugins: [
+      analog({
+        nitro: {
+          output: Object.assign({
+            dir: resolve(process.cwd(), 'dist/client/analog'),
+            publicDir: resolve(process.cwd(), 'dist/client/analog'),
+          }),
+        },
+      }),
+      tsConfigPaths({
+        root: '../',
+      }),
+      {
+        name: 'test',
+        transform(code, id) {
+          if (code.includes('os.type()')) {
+            return {
+              code: code.replace('os.type()', `''`).replace('os.arch()', `''`),
+            };
+          }
+          return;
+        },
       },
-    }),
-    splitVendorChunkPlugin(),
-    tsconfigPaths({
-      root: '../',
-    }),
-    {
-      name: 'test',
-      transform(code, id) {
-        if (code.includes('os.type()')) {
-          return {
-            code: code.replace('os.type()', `''`).replace('os.arch()', `''`),
-          };
-        }
-        return;
+      {
+        name: 'global',
+        transform(code, id) {
+          if (code.includes('global') && id.includes('platform-server.mjs')) {
+            return {
+              code: code
+                .replaceAll('global.', 'globalThis.')
+                .replaceAll('global,', 'globalThis,')
+                .replaceAll(' global[', ' globalThis['),
+            };
+          }
+          return;
+        },
+      },
+    ],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+      include: ['**/*.spec.ts'],
+      cache: {
+        dir: `../../node_modules/.vitest`,
       },
     },
-    {
-      name: 'global',
-      transform(code, id) {
-        if (code.includes('global') && id.includes('platform-server.mjs')) {
-          return {
-            code: code
-              .replaceAll('global.', 'globalThis.')
-              .replaceAll('global,', 'globalThis,')
-              .replaceAll(' global[', ' globalThis['),
-          };
-        }
-        return;
-      },
+    define: {
+      'import.meta.vitest': mode !== 'production',
+      'process.versions.node': `'node'`,
+      'process.versions.v8': `'v8'`,
     },
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['src/test.ts'],
-    include: ['**/*.spec.ts'],
-  },
-  define: {
-    'import.meta.vitest': mode !== 'production',
-    'process.versions.node': `'node'`,
-    'process.versions.v8': `'v8'`,
-  },
-}));
+  };
+});
