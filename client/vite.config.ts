eed9a974137c557357d413f23d9cd49949b8c94e
jaybell <jaycooperbell@gmail.com>
2023-09-24 21:44:01 -0400

    build: fix tsconfig paths import

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import analog from '@analogjs/platform';
 import { resolve } from 'node:path';
-import tsconfigPaths from 'vite-tsconfig-paths/dist';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
