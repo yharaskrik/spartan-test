78982ebeca88cf9a1e4993f994915e55ccc60cf6
jaybell <jaycooperbell@gmail.com>
2023-09-29 17:55:51 -0400

    deploy with langchain added

--- client/src/server/routes/v1/hello.ts
+++ client/src/server/routes/v1/hello.ts
@@ -1,10 +1,29 @@
 import { defineEventHandler } from 'h3';
+import { BufferMemory } from 'langchain/memory';
+import { CloudflareD1MessageHistory } from 'langchain/stores/message/cloudflare_d1';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { ConversationChain } from 'langchain/chains';
 
-export default defineEventHandler((event) => {
-  console.log(globalThis);
-  console.log(globalThis.__env__);
+export default defineEventHandler(async (event) => {
+  const input = new URL(event.path).searchParams.get('input');
+
+  console.log(input);
+
+  const memory = new BufferMemory({
+    chatHistory: new CloudflareD1MessageHistory({
+      tableName: 'stored_message',
+      sessionId: 'sessionId',
+      database: globalThis.__env__.DB,
+    }),
+  });
+
+  const model = new ChatOpenAI({
+    openAIApiKey: globalThis.__env__.OPENAI_API_KEY,
+  });
+  const chain = new ConversationChain({ llm: model, memory });
+  const res = await chain.call({ input });
 
   return {
-    d1: event.context,
+    response: res.response,
   };
 });
